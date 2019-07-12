3420b571bbad3531a82dc73e2cfef3489c6c420b
Chris <christopher.gilroy2@gmail.com>
2019-07-12 15:09:59 +0100

    Committing final version of dependent-assertions branch - 12/07/2019

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled(value = "Disabled until we learn Mocking")
+public class OwnerSDJpaServiceTest {
+
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Disabled
+    @Test
+    void findByLastName() {
+        Owner foundOwner = service.findByLastName("Gilroy");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
