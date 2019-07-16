a046b043d9a6b20895523596e8964df459bdb80e
Chris <christopher.gilroy2@gmail.com>
2019-07-16 21:37:12 +0100

    Committing final version of timeout_chris branch - 16/07/2019

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,4 +35,21 @@ class IndexControllerTest {
             controller.oopsHandler();
         });
     }
+
+    @Disabled("Demo of timeout")
+    @Test
+    public void testTimeOut() {
+        assertTimeout(Duration.ofMillis(100), () -> Thread.sleep(5000));
+
+        System.out.println("I got here - testTimeOut");
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    public void testTimeOutPreempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> Thread.sleep(5000));
+
+        System.out.println("I got here - testTimeOutPreempt");
+    }
+
 }
